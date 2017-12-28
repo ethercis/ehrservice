82b6e0a5093572b8772ce862aa1300374b83e162
serefarikan <serefarikan@gmail.com>
2017-12-28 18:53:15 +0000

    modify test to use test resources folders

--- core/src/test/java/com/ethercis/ehr/encode/TreeMapBindingTest.java
+++ core/src/test/java/com/ethercis/ehr/encode/TreeMapBindingTest.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openehr.rm.composition.Composition;
 
+import java.io.File;
 import java.io.FileReader;
 import java.util.Map;
 import java.util.Properties;
@@ -19,13 +20,17 @@ public class TreeMapBindingTest extends TestCase {
 
     I_KnowledgeCache knowledge;
     Composition composition;
+    private String resourcesRootPath;
 
     @Before
     public void setUp() throws Exception {
+
+        setResourcesRootPath();
+
         Properties props = new Properties();
-        props.put("knowledge.path.archetype", "/Development/Dropbox/eCIS_Development/knowledge/production/archetypes");
-        props.put("knowledge.path.template", "/Development/Dropbox/eCIS_Development/knowledge/production/templates");
-        props.put("knowledge.path.opt", "/Development/Dropbox/eCIS_Development/knowledge/production/operational_templates");
+        props.put("knowledge.path.archetype", resourcesRootPath + "/knowledge/archetypes");
+        props.put("knowledge.path.template", resourcesRootPath + "/knowledge/templates");
+        props.put("knowledge.path.opt", resourcesRootPath + "/knowledge/operational_templates");
         knowledge = new KnowledgeCache(null, props);
 
         Pattern include = Pattern.compile(".*");
@@ -38,9 +43,23 @@ public class TreeMapBindingTest extends TestCase {
         composition = contentBuilder.generateNewComposition();
     }
 
+    private void setResourcesRootPath() {
+        resourcesRootPath = getClass()
+            .getClassLoader()
+            .getResource(".")
+            .getFile();
+    }
+
     @Test
     public void testTreeMapBinding() throws Exception {
-        FileReader fileReader = new FileReader("/Development/Dropbox/eCIS_Development/samples/ProblemList_1FLAT.json");
+        File file =
+            new File(
+                getClass()
+                    .getClassLoader()
+                    .getResource("flat_json_input/IDCR Problem List.v1.FLAT.json")
+                    .getFile());
+//        FileReader fileReader = new FileReader("/Development/Dropbox/eCIS_Development/samples/ProblemList_1FLAT.json");
+        FileReader fileReader = new FileReader(file);
 
         Map<String, String> inputMap = FlatJsonUtil.inputStream2Map(fileReader);
 
