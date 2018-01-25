2d305d016c3fd740e4cb1ebcf24f4ecb502c9b32
serefarikan <serefarikan@gmail.com>
2018-01-25 11:57:58 +0000

    update report name format

--- push_reports.sh
+++ push_reports.sh
@@ -17,7 +17,7 @@ export COMMIT_ID=$(git rev-parse HEAD)
 export COMMIT_SHORT=${COMMIT_ID:0:8}
 export BUILD_DATE_TIME=$(git rev-parse HEAD | git show -s --format=%ci)
 
-sed -i 's^<!--template-->^<!--template-->\n<a href="https://github.com/serefarikan/ehrservice/commit/'"$COMMIT_ID"'">'"$COMMIT_SHORT"'</a><span> :  ('"$BUILD_DATE_TIME"')</span> <a href="site_'"$COMMIT_SHORT"'/surefire-report.html">Surefire test reports</a> <a href="https://travis-ci.org/serefarikan/ehrservice/builds/'"$TRAVIS_BUILD_ID"'">Travis CI build</a> <a href="https://api.travis-ci.org/v3/job/'"$TRAVIS_JOB_ID"'/log.txt">Travis CI build log(raw)</a><br>^g' travis-reports/index.html
+sed -i 's^<!--template-->^<!--template-->\n<a href="https://github.com/serefarikan/ehrservice/commit/'"$COMMIT_ID"'">'"$COMMIT_SHORT"' (CORE)</a><span> :  ('"$BUILD_DATE_TIME"')</span> <a href="site_'"$COMMIT_SHORT"'/surefire-report.html">Surefire test reports</a> <a href="https://travis-ci.org/serefarikan/ehrservice/builds/'"$TRAVIS_BUILD_ID"'">Travis CI build</a> <a href="https://api.travis-ci.org/v3/job/'"$TRAVIS_JOB_ID"'/log.txt">Travis CI build log(raw)</a><br>^g' travis-reports/index.html
 
 cd travis-reports/ # descend into temp git repo dir so that you can push to reports github page
 cp ../target/site -r ./site_$COMMIT_SHORT
