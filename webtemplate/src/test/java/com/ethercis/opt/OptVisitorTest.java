f2696f74540fdad8e0292f6dcbea4265a0204308
Christian Chevalley <christian@adoc.co.th>
2018-04-20 09:47:24 +0700

    Fixed test

--- webtemplate/src/test/java/com/ethercis/opt/OptVisitorTest.java
+++ webtemplate/src/test/java/com/ethercis/opt/OptVisitorTest.java
@@ -60,8 +60,8 @@ public class OptVisitorTest extends TestCase {
         String expectedJson = new String(Files.readAllBytes(Paths.get("src/test/resources/RIPPLE-Conformance Test Introspected.json")));
 
 //        OPERATIONALTEMPLATE operationaltemplate = (OPERATIONALTEMPLATE)knowledge.retrieveTemplate("IDCR Allergies List.v0");
-        OPERATIONALTEMPLATE operationaltemplate = (OPERATIONALTEMPLATE)knowledge.retrieveTemplate("IDCR Problem List.v1");
-//        OPERATIONALTEMPLATE operationaltemplate = (OPERATIONALTEMPLATE)knowledge.retrieveTemplate("RIPPLE - Conformance Test template");
+//        OPERATIONALTEMPLATE operationaltemplate = (OPERATIONALTEMPLATE)knowledge.retrieveTemplate("IDCR Problem List.v1");
+        OPERATIONALTEMPLATE operationaltemplate = (OPERATIONALTEMPLATE)knowledge.retrieveTemplate("RIPPLE - Conformance Test template");
         Map map = new OptVisitor().traverse(operationaltemplate);
 
         assertNotNull(map);
